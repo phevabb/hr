1c6c256a31b56a903ddfaddb05c37b093f8e02cb
phevabb <phevab1@gmail.com>
2026-06-14 01:34:13 +0000

    origin edit

--- A_sys/settings.py
+++ A_sys/settings.py
@@ -201,6 +201,7 @@ EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = "phevab1@gmail.com"
 CSRF_TRUSTED_ORIGINS = [
     "https://hr-production-415c.up.railway.app",
+    "https://humanresourcebackend-7ritgqhp.b4a.run",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
